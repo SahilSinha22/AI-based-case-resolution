c28021917acbe7bcfdc3255fe0cfeb8bb6f72e26
sahil sinha <sahilsinha2204@gmail.com>
2024-07-17 13:31:39 +0530

    Comp5

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import Form from '@/components/Form'
+
+import Footer from "@/components/Footer";
+const page = () => {
   return (
     <div>
      
-    
+     
+      <Form/>
+      <Footer/>
     </div>
   )
 }
